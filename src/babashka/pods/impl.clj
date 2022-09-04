8106c648f0bc8d96ce1b2bcc8114e3e22a0c81cc
Nate Jones <nate@endot.org>
2022-09-04 22:17:35 +0000

    Add pod reply fields for print and flush

--- src/babashka/pods/impl.clj
+++ src/babashka/pods/impl.clj
@@ -213,10 +213,19 @@
                                                  chan)
                     out (some-> (get reply "out")
                                 bytes->string)
+                    pod-print (some-> (get reply "print")
+                                      bytes->string)
+                    pod-flush (contains? reply "flush")
                     err (some-> (get reply "err")
                                 bytes->string)]
                 ;; NOTE: write to out and err before delivering promise for making
                 ;; listening to output synchronous.
+                (when pod-print
+                  (binding [*out* out-stream]
+                    (print pod-print)))
+                (when pod-flush
+                  (binding [*out* out-stream]
+                    (. out-stream (flush))))
                 (when out
                   (binding [*out* out-stream]
                     (println out)))
